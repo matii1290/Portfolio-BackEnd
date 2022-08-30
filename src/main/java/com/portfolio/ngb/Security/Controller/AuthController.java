a89078a13695f9b2c494625deab38653b1b120ce
matii1290 <matu1234512@gmail.com>
2022-08-30 14:39:36 -0300

    corrigiendo cors en el back

--- src/main/java/com/portfolio/ngb/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/ngb/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://frontendportfolio001.web.app")
 public class AuthController {
 
     @Autowired
